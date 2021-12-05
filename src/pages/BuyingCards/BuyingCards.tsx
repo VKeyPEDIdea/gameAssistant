0b9c224b66da339be8edce95e4f094212c6442fb
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2021-12-05 14:36:34 +0600

    add header to page

--- src/pages/BuyingCards/BuyingCards.tsx
+++ src/pages/BuyingCards/BuyingCards.tsx
@@ -9,6 +9,7 @@ import {
 	selectMatchRound,
 } from "../../features/match/matchSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import classes from './BuyingCards.module.scss';
 
 const BuyingCards = () => {
 	const dispatch = useAppDispatch();
@@ -37,6 +38,7 @@ const BuyingCards = () => {
 				roundCount={3}
 				roundCurrent={round}
 				duration={duration}/>
+			<h1 className={classes.header}>Покупка карточек</h1>
 		</>
 	);
 };
